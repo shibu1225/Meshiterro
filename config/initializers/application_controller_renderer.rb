454158d5886c532c96470111850bd3019d64be77
shibu1225 <keisuke.shibusawa@gmail.com>
2021-12-21 14:19:11 +0000

    12/21 commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '02a740ce89482d0e02cde233258fe5ef536a5a181ab83c2419a6f78a0205ab0b7900657c0acb8dd3774bdc97b34d4f20f55d303b70520dc61dee8c025fb42840'
